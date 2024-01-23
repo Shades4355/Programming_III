f5d0ae96b7e1ace514cb071407eb544e9bfb693b
Shades <51397439+Shades4355@users.noreply.github.com>
2024-01-22 19:58:55 -0500

    More gitignore updates (#3)
    * created more Hello World files; 2 didn't generate the right files; 3 did
    
    * updated gitignore
    
    * deleted improperly created files
    
    * added header to HelloWorld

--- pre-class_testing/HelloWorld.java
+++ pre-class_testing/HelloWorld.java
@@ -1,3 +1,15 @@
+// File name:   HelloWorld
+// Written by:  Shades Meyers
+// Description: a simple "Hello, World!" file to test my Java installation
+// Challenges:  getting Java to work on my machine
+// Time Spent:  mere minutes to create the file; half hour+ to get Java working
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Jan-17  SM      File created
+
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
