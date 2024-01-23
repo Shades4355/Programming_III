be32aeb20ac01542c61cbc9a3d8af1654f4314c6
Shades <shades4355@gmail.com>
2024-01-23 12:35:39 -0500

    worked on main file - WIP
    * break for class

--- review_assignment/TestMyBoundedShapes.java
+++ review_assignment/TestMyBoundedShapes.java
@@ -3,13 +3,46 @@
 // Description: main file for executing assignment. Used to create various
 //          shapes and determine if their methods are functioning correctly
 // Challenges:  
-// Time Spent:  52 minutes + 
+// Time Spent:  52 minutes + 29 minutes
 //
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-Jan-23  SM      Files created
 
+import java.util.ArrayList;
+
+
 public class TestMyBoundedShapes {
+    public static void main(String[] args) {
+        
+        // create subclass MyCircle objects
+        MyCircle oval1 = new MyCircle();
+        MyCircle oval2 = new MyCircle(5);
+        MyCircle oval3 = new MyCircle(8.1, "red", false);
+
+        // create subclass MyRectangle objects
+        MyRectangle rectangle1 = new MyRectangle();
+        MyRectangle rectangle2 = new MyRectangle(12, 4);
+        MyRectangle rectangle3 = new MyRectangle(5.5, 2.0, "black", true);
     
+        // initializing and populating an ArrayList
+        ArrayList<MyBoundedShape> myBoundedShapes = new ArrayList<MyBoundedShape>();
+        myBoundedShapes.add(oval1);
+        myBoundedShapes.add(oval2);
+        myBoundedShapes.add(oval3);
+        myBoundedShapes.add(rectangle1);
+        myBoundedShapes.add(rectangle2);
+        myBoundedShapes.add(rectangle3);
+        
+        // iterate through the ArrayList and print out relevant information
+        for (int i = 0; i < myBoundedShapes.size(); i++) {
+            if () { // if object type is NOT MyCircle, print object
+
+            } else { // if object type IS MyCircle, call printCircle() method
+
+            }
+        }
+
+    }
 }
