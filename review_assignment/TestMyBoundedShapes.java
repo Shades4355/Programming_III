f39da2959aaa27677c31fb7ab3aa71b677b15a1a
Shades <shades4355@gmail.com>
2024-01-23 15:45:32 -0500

    Code still borked; taking a break

--- review_assignment/TestMyBoundedShapes.java
+++ review_assignment/TestMyBoundedShapes.java
@@ -5,7 +5,9 @@
 // Challenges:  My biggest challenge in this file was getting the if/else comparison to work.
 //          I did not remember covering this type of object comparison in Programming II,
 //          so I had to do extensive Googling to find the getClass() syntax
-// Time Spent:  52 minutes + 29 minutes + 
+//          An additional challenge was figuring out how to call subclass methods when the program thought
+//          they were the parent class, which lacks said methods
+// Time Spent:  52 minutes + 29 minutes + 2h 25m + 
 //
 // Revision history:
 // Date:        By:     Action:
@@ -38,21 +40,42 @@ public class TestMyBoundedShapes {
         myBoundedShapes.add(rectangle3);
         
         // iterate through the ArrayList and print out relevant information
-        MyCircle circleTester = new MyCircle();
-        MyRectangle rectTester = new MyRectangle();
-        for (MyBoundedShape i : myBoundedShapes) {
-            if (i.getClass().equal(rectTester.getClass())) { // if object type is NOT MyCircle, print object (toString)
-                System.out.println(
-                    "This is a " + i.getClass() +
-                    "\n" + i.toString() +
-                    "\narea: " + i.getArea() +
-                    "\nperimeter: " + getPerimeter()
+        MyCircle circleTester = new MyCircle();     // create a dummy circle object for comparing getClass()
+        MyRectangle rectTester = new MyRectangle(); // create a dummy rectangle object for comparing getClass()
+        for (int i = 0; i < myBoundedShapes.size(); i++) {
+            if (myBoundedShapes.get(i).getClass().equals(rectTester.getClass())) {
+                System.out.printf(
+                    "This is a %s\n%s\n\nwidth: %d.1f\nheight: %d.1f\narea: %d.2f\nperimeter: %d.2f", myBoundedShapes.get(i).getClass(),  myBoundedShapes.get(i).toString(), myBoundedShapes.get(i).getWidth(), myBoundedShapes.get(i).getHeight(), myBoundedShapes.get(i).getArea(), myBoundedShapes.get(i).getPerimeter()
                 );
-            } else if (i.getClass().equals(circleTester.getClass())) { // if object type IS MyCircle, call printCircle() method
-                i.printCircle();
-            } else {
-                System.out.println("ERROR: incorrect class type detected:" + i.getClass());
-            }
-        }
-    }
-}
+            } else if (myBoundedShapes.get(i).getClass().equals(circleTester.getClass())) {
+                myBoundedShapes.get(i).printCircle();
+            } else { // if class is not MyCircle or MyRectangle, then an error has ocurred
+                System.out.println("ERROR: unrecognized class: " + myBoundedShapes.get(i).getClass());
+                System.exit(1); // exit program with error code 1
+            } // end If
+        } // end For loop
+        
+        // for (MyBoundedShape i : myBoundedShapes) {
+            // if (i.getClass().equal(rectTester.getClass())) { // if object type is NOT MyCircle, print object (toString)
+            //     System.out.println(
+            //         "This is a " + i.getClass() +
+            //         "\n" + i.toString() +
+            //         "\narea: " + i.getArea() +
+            //         "\nperimeter: " + getPerimeter()
+            //     );
+            // } else if (i.getClass().equals(circleTester.getClass())) { // if object type IS MyCircle, call printCircle() method
+            //     i.printCircle();
+            //     System.out.println(
+            //         "\nradius: " + i.getRadius() +
+            //         "\ndiameter: " + i.getDiameter() +
+            //         "\narea: " + i.getArea() +
+            //         "\nperimeter: " + i.getPerimeter()
+            //         );
+            // } else {
+            //     System.out.println("ERROR: incorrect class type detected:" + i.getClass());
+            // }
+        // } // end For loop
+
+        System.exit(0); // exit program successfully
+    } // end Main method
+} // end class
