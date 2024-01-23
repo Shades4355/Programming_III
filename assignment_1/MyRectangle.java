09e1498dc858e57ebc66a4175acec1c3863e7697
Shades <shades4355@gmail.com>
2024-01-23 11:32:41 -0500

    created MyRectangle

---
+++ assignment_1/MyRectangle.java
@@ -0,0 +1,44 @@
+public class MyRectangle extends MyBoundedShape {
+    // private variables
+    private double width;
+    private double height;
+
+    // constructors
+    public MyRectangle() {
+        width = 1.0;
+        height = 2.0;
+    }
+    public MyRectangle(double w, double h) {
+        width = w;
+        height = h;
+    }
+    public MyRectangle(double w, double h, String c, boolean f) {
+        super(c, f);
+        width = w;
+        height = h;
+    }
+
+    // Accessors and Mutators, grouped by variable
+    // Width
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double w) {
+        width = w;
+    }
+    // Height
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double h) {
+        height = h;
+    }
+
+    // Rectangle specific methods
+    public double getArea() {
+        return width * height;
+    }
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
