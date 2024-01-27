b50efafaad809bd81139f14e43fccad4c509f873
Shades <shades4355@gmail.com>
2024-01-27 12:54:30 -0500

    updated comments for additional clarity
    * Added additional comments
    
    * updated time in Header
    
    * included github link
    
    * updated line # in comment to reflect its change

--- review_assignment/TestMyBoundedShapes.java
+++ review_assignment/TestMyBoundedShapes.java
@@ -7,7 +7,8 @@
 //          so I had to do extensive Googling to find the instanceof syntax
 //          An additional big challenge was figuring out how to call subclass methods when the program thought
 //          they were the parent class, which lacks said methods (solution: downcasting)
-// Time Spent:  4h 58m
+// Time Spent:  5h 4m
+// code repository: https://github.com/Shades4355/Programming_III/tree/main/review_assignment
 //
 // Revision history:
 // Date:        By:     Action:
@@ -15,6 +16,7 @@
 // 2024-Jan-23  SM      Files created
 // 2024-Jan-27  SM      Minor Formatting changes to print out
 //                      Minor change to line 45 (from "for i..." to "for shape in myBoundedShapes")
+//                      Added additional comments and added clarification to some comments
 
 import java.util.ArrayList;
 
@@ -32,18 +34,18 @@ public class TestMyBoundedShapes {
         MyRectangle rectangle2 = new MyRectangle(12, 4); // constructor with 2 arguments
         MyRectangle rectangle3 = new MyRectangle(5.5, 2.0, "black", true); // constructor with 4 arguments
     
-        // initializing and populating an ArrayList
+        // initializing and populating ArrayList myBoundedShapes
         ArrayList<MyBoundedShape> myBoundedShapes = new ArrayList<MyBoundedShape>();
-        myBoundedShapes.add(oval1);
+        myBoundedShapes.add(oval1); // add ovals (circles) to ArrayList
         myBoundedShapes.add(oval2);
         myBoundedShapes.add(oval3);
-        myBoundedShapes.add(rectangle1);
+        myBoundedShapes.add(rectangle1); // add rectangles to ArrayList
         myBoundedShapes.add(rectangle2);
         myBoundedShapes.add(rectangle3);
         
-        // iterate through the ArrayList and print out relevant information
+        // iterate through the ArrayList myBoundedShapes and print out relevant information
         for (MyBoundedShape shape : myBoundedShapes) {
-            if (shape instanceof MyRectangle) {
+            if (shape instanceof MyRectangle) { // if shape is a Rectangle, print out information to match given format
                 MyRectangle rectangle = (MyRectangle) shape;
                 System.out.println(rectangle);
                 System.out.println();
@@ -54,7 +56,7 @@ public class TestMyBoundedShapes {
                 );
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 
-            } else if (shape instanceof MyCircle) {
+            } else if (shape instanceof MyCircle) { // if shape is a Circle, print out information to match given format
                 MyCircle circle = (MyCircle) shape;
                 System.out.println(circle);
                 System.out.println();
@@ -76,7 +78,7 @@ public class TestMyBoundedShapes {
 
         for (int i = 0; i < myBoundedShapes.size(); i++) {
             System.out.printf("BoundedShape object %d is a %s%n", i + 1, myBoundedShapes.get(i).getClass().getName());
-        }
+        } // end For loop
 
         System.exit(0); // exit program successfully
     } // end Main method
