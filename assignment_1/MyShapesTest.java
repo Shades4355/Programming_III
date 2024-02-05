d1a3e865a6dfba8eecbc7b8bf7feff9b4dd3917b
Shades <shades4355@gmail.com>
2024-02-05 17:36:52 -0500

    almost done typing print statements

--- assignment_1/MyShapesTest.java
+++ assignment_1/MyShapesTest.java
@@ -1,78 +1,103 @@
-//import any necessaary package here
-
-//For MyShapesTest code documentation
 /**
  * File name: MyShapesTest
- * Written by: outline by Prof. Yeung & final touches by Shades Meyers
+ * Written by: outline by Prof. Yeung & final details by Shades Meyers
  * 
- * Description:
+ * Description: a program that creates, compares, and describes various shapes.
  * Challenges:
  *
  * Time Spent:
  * 
  * Revision History:
- * Date: By: Action:
+ * Date:            By:     Action:
  * ---------------------------------------------------
- * 2024-Feb-05 SM Downloaded MyShapesTest.java from Blackboard
+ * 2024-Feb-05      SM      Downloaded MyShapesTest.java from Blackboard
+ *                          Altered class names to be inline with the assignment instructions (ex: Circle -> MyCircle)
  * 
  */      
 
+ 
+ import java.util.ArrayList;
+
 
 public class MyShapesTest  {
-    
   public static void main(String[] args) { 
-
     // Create two comparable circles
-    Circle c1 = new Circle(3.5);
-    Circle c2 = new Circle(10, "BLUE", true);
+    MyCircle c1 = new MyCircle(3.5);
+    MyCircle c2 = new MyCircle(10, "BLUE", true);
     // Create two comparable rectangles
-    Rectangle r1 = new Rectangle(25,34);
-    Rectangle r2 = new Rectangle(12,20,"YELLOW",true);
+    MyRectangle r1 = new MyRectangle(25,34);
+    MyRectangle r2 = new MyRectangle(12,20,"YELLOW",true);
     // Create two comparable squares    
-    Square sq1 = new Square(10);
-    Square sq2 = new Square(24.2,"RED", true);
+    MySquare sq1 = new MySquare(10);
+    MySquare sq2 = new MySquare(24.2,"RED", true);
     // Create two comparable triangles    
-    Triangle tr1 = new Triangle(2, 3, 4);
-    Triangle tr2= new Triangle(10, 20.5, 30.23, "GREEN", true);
+    MyTriangle tr1 = new MyTriangle(2, 3, 4);
+    MyTriangle tr2= new MyTriangle(10, 20.5, 30.23, "GREEN", true);
  
     
     // Writing codes here:
     //Enter codes to add the above eight objects to an ArrayList of MyBoundedShape 
     // ArrayList of MyBoundedShape objects that will be used to polymorphically store shapes
+    ArrayList<MyBoundedShape> myShapes = new ArrayList<MyBoundedShape>();
+    myShapes.add(c1);
+    myShapes.add(c2);
+
+    myShapes.add(r1);
+    myShapes.add(r2);
+
+    myShapes.add(sq1);
+    myShapes.add(sq2);
+
+    myShapes.add(tr1);
+    myShapes.add(tr2);
 
     // Utilize a loop by invoking the necessary methods to print the following message for each object:
     // The object's name, String description of Objects, its area and perimeter with two decimal places, 
     //and how to draw this object
+    int objCount = 0;
+    for (MyBoundedShape shape: myShapes) {
+        objCount += 1;
+        System.out.println("The object%d is: %s".formatted(objCount, shape.getName()));
+        System.out.println(shape);
 
-    /* For example:
-    The object1 is: Circle
-    created on Sun Sep 10 15:10:09 EDT 2023
-    color: white and filled: false
-    radius: 3.5
+        System.out.println(""); // print a blank line
 
-    Area is 38.48
-    Perimeter is 21.99
-    How to draw this object: Color with the radius
-    ------------------------------------
-    The object2 is: Circle
-    created on Sat Feb 03 00:32:44 EST 2024
-    color: BLUE and filled: true
-    radius: 10.0
+        System.out.println("Area is %d.2f".formatted(shape.getArea()));
+        System.out.println("Perimeter %d.2f".formatted(shape.getPerimeter()));
+        System.out.println("How to draw this object: %s".formatted(shape.howToDraw()));
 
-    Area is 314.16
-    Periemeter is 62.83
-    How to draw this object: Color with the radius
-    ------------------------------------
-    The object3 ....
-    */
-            
-    System.out.println("------------------------------------\n"); //Use it to divide each object
+        /*
+        * For example:
+        * The object1 is: Circle
+        * created on Sun Sep 10 15:10:09 EDT 2023
+        * color: white and filled: false
+        * radius: 3.5
+        * 
+        * Area is 38.48
+        * Perimeter is 21.99
+        * How to draw this object: Color with the radius
+        * ------------------------------------
+        * The object2 is: Circle
+        * created on Sat Feb 03 00:32:44 EST 2024
+        * color: BLUE and filled: true
+        * radius: 10.0
+        * 
+        * Area is 314.16
+        * Perimeter is 62.83
+        * How to draw this object: Color with the radius
+        * ------------------------------------
+        * The object3 ....
+        */
+
+        System.out.println("------------------------------------\n"); // Use it to divide each object
+    }
+
+    
     
     //comparison of two objects with the same shapes
     System.out.println("\nComparison of two objects with the same shapes: \n"); 
 
     System.out.println("Comparing two circle objects: ");
-
     //using equals, compareTo and max methods to compare two circle objects
     /* For example:
     Comparing two circle objects: 
@@ -82,10 +107,14 @@ public class MyShapesTest  {
     color: BLUE and filled: true
     radius: 10.0
     */
-    //writing codes here    
-        System.out.println("------------------------------------\n"); //Use it to divide each object
+    //writing codes here   
+    System.out.println("object1 equals object2: %b".formatted(c1.equals(c2)));
+    System.out.println("object1 compares to object2: %d".formatted(c1.compareTo(c2)));
+    System.out.println("The max geometric circle object is %s".formatted(MyBoundedShape.max(c1, c2)));
+
+    System.out.println("------------------------------------\n"); //Use it to divide each object
         
-        System.out.println("Comparing two rectangle objects: ");
+    System.out.println("Comparing two rectangle objects: ");
     //using equals, compareTo and max methods to compare two rectangle objects
     /* For example:
     Comparing two rectangle objects: 
@@ -97,9 +126,13 @@ public class MyShapesTest  {
     ------------------------------------
     */
     //writing codes here
-        System.out.println("------------------------------------\n"); //Use it to divide each object
+    System.out.println("object3 equals object4: %b".formatted(r1.equals(r2)));
+    System.out.println("object3 compares to object4: %d".formatted(r1.compareTo(r2)));
+    System.out.println("The max geometric circle object is %s".formatted(MyBoundedShape.max(r1, r2)));
 
-        System.out.println("Comparing two square objects: ");
+    System.out.println("------------------------------------\n"); //Use it to divide each object
+
+    System.out.println("Comparing two square objects: ");
     //using equals, compareTo and max methods to compare two square objects  
     /* For example:
     Comparing two square objects: 
@@ -110,9 +143,13 @@ public class MyShapesTest  {
     side: 24.2
     ------------------------------------
     */
-        System.out.println("------------------------------------\n"); //Use it to divide each object
+    System.out.println("object5 equals to object6: %b".formatted(sq1.equals(sq2)));
+    System.out.println("object5 compares to object6: %d".formatted(sq1.compareTo(sq2)));
+    System.out.println("The max geometric square object is %s".formatted(MyBoundedShape.max(sq1, sq2)));
+
+    System.out.println("------------------------------------\n"); //Use it to divide each object
         
-        System.out.println("Comparing two triangle objects: ");
+    System.out.println("Comparing two triangle objects: ");
     //using equals, compareTo and max methods to compare two triangle objects   
     /*
     For example: 
@@ -126,7 +163,7 @@ public class MyShapesTest  {
     side3 = 30.2
     */
     //writing codes here
-        System.out.println("------------------------------------\n");//Use it to divide each object
+    System.out.println("------------------------------------\n");//Use it to divide each object
         
     }//end main method
 }//end class
