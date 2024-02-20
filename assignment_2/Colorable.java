5c42991288b06db13cbee913a262868d350dd3b8
Shades <shades4355@gmail.com>
2024-02-20 10:17:42 -0500

    added Colorable and MyBoundedShape to project folder

---
+++ assignment_2/Colorable.java
@@ -0,0 +1,16 @@
+// File name:   Colorable
+// Written by:  Shades Meyers
+// Description: an interface that implements 2 methods: Colorable() and Comparable()
+// Challenges:  None
+// Time Spent:  0 minutes
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Feb-20  SM      Copied file from Assignment 1
+
+
+public interface Colorable {
+    public abstract String howToDraw();
+    public abstract String getName();
+}
