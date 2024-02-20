8358ad632c77cb935741434ecba70e2d5f565e14
Shades <shades4355@gmail.com>
2024-02-20 08:59:32 -0500

    updated MySquare from Assignment 1 based on Assignment 2

---
+++ assignment_2/MySquare.java
@@ -0,0 +1,75 @@
+// File name:   MySquare
+// Written by:  Shades Meyers
+// Description: Creates a child of MyBoundedShape, a Square,
+//              which throws an exception if its side is less than or equal to 0
+// Challenges:  None
+// Time Spent:  4 minutes
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Feb-20  SM      Copied and modified contents of MySquare.java from Assignment 1
+
+
+public class MySquare extends MyBoundedShape {
+    // variables
+    private double side;
+
+    // Constructors
+    public MySquare() {
+        super();
+        side = 1.0;
+    }
+
+    public MySquare(double side) throws IllegalArgumentException {
+        super();
+        setSide(side);
+    }
+
+    public MySquare(double side, String color, boolean filled) throws IllegalArgumentException {
+        super(color, filled);
+        setSide(side);
+    }
+
+    // Accessors and Mutators
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) throws IllegalArgumentException {
+       if (side <= 0) {
+        throw new IllegalArgumentException();
+       } else {
+           this.side = side;
+       }
+    }
+
+    // Implemented methods from MyBoundedShape
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    // Implemented methods from Colorable
+    @Override
+    public String getName() {
+        return "Square";
+    }
+
+    @Override
+    public String howToDraw() {
+        return "Color with four sides";
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        return super.toString() + "%nside: %.1f".formatted(side);
+    }
+
+} // end of program
