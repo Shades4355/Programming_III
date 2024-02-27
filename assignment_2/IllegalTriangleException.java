8c46912da0cdacdd8d620c6a720cf7bb13cdb965
Shades <shades4355@gmail.com>
2024-02-27 13:32:56 -0500

    fixed illegalTriangle exception

--- assignment_2/IllegalTriangleException.java
+++ assignment_2/IllegalTriangleException.java
@@ -2,12 +2,13 @@
 // Written by: Shades Meyers
 // Description: a custom exception for when a triangle has illegal side lengths
 // Challenges: None
-// Time Spent: 8 minutes
+// Time Spent: 9 minutes
 //
 // Revision history:
 // Date:        By:     Action:
 // -------------------------------
 // 2024-Feb-20  SM      File created
+// 2024-Feb-27  SM      Added missing components to second Constructor
 
 
 public class IllegalTriangleException extends Exception {
@@ -17,7 +18,10 @@ public class IllegalTriangleException extends Exception {
         super("Invalid triangle: Sum of any two sides is not greater than the third side.");
     }
     public IllegalTriangleException(double side1, double side2, double side3, String error) {
-        super("Invalid triangle: " + error);
+        super("Invalid triangle: " + error
+                + "\nSide 1: " + Double.toString(side1)
+                + "\nSide 2: " + Double.toString(side2)
+                + "\nSide 3: " + Double.toString(side3));
     }
 
 } // end program
