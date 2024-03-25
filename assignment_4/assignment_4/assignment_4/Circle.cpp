aaf6b2d3ad95303e0f5d0addaea0bff2a08447ab
Shades <shades4355@gmail.com>
2024-03-25 16:17:19 -0400

    Circle class started

---
+++ assignment_4/assignment_4/assignment_4/Circle.cpp
@@ -0,0 +1,16 @@
+// File name:   GeometricObject.cpp
+// Written by:  Shades Meyers
+// Description: A Circle class
+// Challenges:  None
+//                  
+// Time Spent:   5 min
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Mar-25  SM      File created
+
+
+#include "Circle.h"
+
+
---
+++ assignment_4/assignment_4/assignment_4/Circle.h
@@ -0,0 +1,49 @@
+// File name:   GeometricObject.cpp
+// Written by:  Shades Meyers
+// Description: The header for a Circle class
+// Challenges:  None
+//                  
+// Time Spent:   
+//
+// Revision history:
+// Date:        By:     Action:
+// -------------------------------
+// 2024-Mar-25  SM      File created
+
+
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+
+#include "GeometricObject.h"
+
+
+class Circle : public GeometricObject {
+private:
+    double radius;
+
+public:
+    // Constructors
+    Circle();
+    Circle(double radius);
+    Circle(double radius, string color, bool filled);
+
+    // Destructor
+    ~Circle();
+
+    // Accessors and Mutators
+    // Radius
+    double getRadius() const;
+    void setRadius(double radius);
+
+    // Diameter
+    double getDiameter() const;
+
+    // Area
+    virtual double getArea() const override;
+
+
+};
+
+#endif
+
